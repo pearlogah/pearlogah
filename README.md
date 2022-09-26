ee112115fd7dfe97a18078d41ca8030091760593
elizabeth ogah <you@example.com>
2022-09-26 12:06:32 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # New-repo
+# pearlogah
---
+++ pearl.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract pearl{
+
+    
+uint[] numbers;
+
+numbers.push(1);
+numbers.push(2);
+numbers.push(3); 
+numbers.push(4);
+numbers.push(5);
+
+
+
+
+
+}
